436b4b705a376d9f83c0ba8a0434dda4272be134
Marc Vañó Villanueva <mvaovil@edu.upv.es>
2024-09-24 20:11:22 +0200

    Añadida implementación de la clase ListArray

--- ListArray.hpp
+++ ListArray.hpp
@@ -1,18 +1,20 @@
 #include <ostream>
-#include "List.h"
+#include "List.hpp"
 
 template <typename T> 
 class ListArray : public List<T> {
 
     private:
-        void resize(int new_size); 
-
+	T* arr;
+	int max; 
+	int n;
+	static const int MINSIZE;
+	void resize(int new_size);
     public:
         ListArray();
 	~ListArray();
 	T operator[](int pos);
 	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
-  	void resize(int new_size);
     
 };
 
